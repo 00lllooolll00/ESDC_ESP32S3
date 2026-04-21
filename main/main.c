1758f3ed54e2a714b96aea5c42ce535e981d7278
N1netyNine99 <n1netynine99@qq.com>
2026-04-21 23:23:18 +0800

    add 添加lvgl作为依赖

--- main/main.c
+++ main/main.c
@@ -1,10 +1,20 @@
+#include "common.h"
 #include "app.h"
+#include "lvgl.h"
+#include "ek_export.h"
 
 void app_main(void)
 {
+    ek_export_init();
     app_init();
 
     while (1)
     {
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+void vApplicationTickHook(void)
+{
+    lv_tick_inc(1);
 }
