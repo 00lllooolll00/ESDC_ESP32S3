454d1e39d65925fb6850c57fa612f05d85172539
N1netyNine99 <n1netynine99@qq.com>
2026-04-22 14:23:24 +0800

    stage 暂存

--- main/main.c
+++ main/main.c
@@ -3,14 +3,16 @@
 #include "lvgl.h"
 #include "ek_export.h"
 
+static const char *TAG = "main.c";
+
 void app_main(void)
 {
     ek_export_init();
-    app_init();
+    // app_init();
 
     while (1)
     {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(100);
     }
 }
 
