2ae2d4ceceb0177dfa9a4c2fdda3235130778897
N1netyNine99 <n1netynine99@qq.com>
2026-04-24 22:36:34 +0800

    update and fix

--- main/main.c
+++ main/main.c
@@ -1,9 +1,17 @@
-#include "common.h"
+#include "user_common.h"
 #include "app.h"
+#include "nvs_flash.h"
 #include "lvgl.h"
 
 void app_main(void)
 {
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
     app_init();
 }
 
