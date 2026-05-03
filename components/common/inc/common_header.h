6421fc9976a69f717deb7bb9a59cdbb88ba8089b
N1netyNine99 <n1netynine99@qq.com>
2026-05-03 20:53:22 +0800

    update 更新 LOG_XXX 显示行号、所在函数

--- components/common/inc/common_header.h
+++ components/common/inc/common_header.h
@@ -18,12 +18,12 @@
 #include "esp_log.h"
 #include "esp_err.h"
 
-#define FILE_TAG(x)      static const char *__LOG_TAG__ = x
+#define FILE_TAG(x)           static const char *__LOG_TAG__ = x
 
-#define LOG_DBUG(...)    ESP_LOGD(__LOG_TAG__, __VA_ARGS__)
-#define LOG_INFO(...)    ESP_LOGI(__LOG_TAG__, __VA_ARGS__)
-#define LOG_WARN(...)    ESP_LOGW(__LOG_TAG__, __VA_ARGS__)
-#define LOG_ERROR(...)   ESP_LOGE(__LOG_TAG__, __VA_ARGS__)
-#define LOG_VERBOSE(...) ESP_LOGV(__LOG_TAG__, __VA_ARGS__)
+#define LOG_DBUG(fmt, ...)    ESP_LOGD(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)    ESP_LOGI(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)    ESP_LOGW(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...)   ESP_LOGE(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_VERBOSE(fmt, ...) ESP_LOGV(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
 
 #endif // COMMON_HEADER_H
