bb6f6c0e6a02d9c027eebab1b391aaf3ccfe927f
N1netyNine99 <n1netynine99@qq.com>
2026-05-05 23:18:56 +0800

    fix and add

--- components/common/inc/common_header.h
+++ components/common/inc/common_header.h
@@ -20,10 +20,29 @@
 
 #define FILE_TAG(x)           static const char *__LOG_TAG__ = x
 
-#define LOG_DBUG(fmt, ...)    ESP_LOGD(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...)   ESP_LOGD(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
 #define LOG_INFO(fmt, ...)    ESP_LOGI(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
 #define LOG_WARN(fmt, ...)    ESP_LOGW(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
 #define LOG_ERROR(fmt, ...)   ESP_LOGE(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
 #define LOG_VERBOSE(fmt, ...) ESP_LOGV(__LOG_TAG__, "[L:%d F:%s]:" fmt, __LINE__, __func__, ##__VA_ARGS__)
+
+FORCE_INLINE_ATTR const char *unified_strerror(int err_code)
+{
+    if (err_code == ESP_OK)
+    {
+        return "ESP_OK (Success)";
+    }
+    if (err_code == ESP_FAIL)
+    {
+        return "ESP_FAIL (Generic Error)";
+    }
+
+    if (err_code > 0 && err_code < 256)
+    {
+        return strerror(err_code);
+    }
+
+    return esp_err_to_name((esp_err_t)err_code);
+}
 
 #endif // COMMON_HEADER_H
