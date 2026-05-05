c4d29722dd5daa0703c93eacb90585a99b3ddd5f
N1netyNine99 <n1netynine99@qq.com>
2026-05-05 10:11:00 +0800

    update

--- components/middlewares/lvgl_v9_5/inc/lv_indev_port.h
+++ components/middlewares/lvgl_v9_5/inc/lv_indev_port.h
@@ -4,5 +4,9 @@
 #include "common_header.h"
 #include "lvgl.h"
 #include "plat_key.h"
+#include "plat_touch.h"
+
+void lv_port_key_init(plat_key_dev_t *key_dev);
+void lv_port_touch_init(plat_touch_dev_t *devs[], uint8_t count);
 
 #endif //  LV_INDEV_PORT_H
